89da31bdc863efb735b4f342e80ca30d165be466
Piers de Greeff <piers.degreeff@gmail.com>
2026-03-06 11:17:49 -0800

    linked matches page. added yes no buttons

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    
+@app.route("/matches")
+def matches():
+    return render_template("matches.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
