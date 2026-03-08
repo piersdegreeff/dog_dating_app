60120ffaf0bd1995a52008413aa2bc3c1d8b17ca
j-ysfn <j.yousefian1@gmail.com>
2026-03-08 01:56:46 -0800

    This contains the Next Dog button function

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,14 @@
+console.log("script.js loaded!")
 
+let cards = document.getElementsByClassName("dog-card");
+let currentDog = 0;
+
+function showNext(){
+    cards[currentDog].computedStyleMap.display = "none";
+    currentDog = currentDog + 1;
+
+    if (currentDog >= cards.length){
+        currentDog = 0;
+    }
+    cards[currentDog].style.display = "block";
+}
